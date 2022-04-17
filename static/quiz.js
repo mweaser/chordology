32c0705da1932ba7c34e03b688efc7b7df980e49
scnappo <sophianappo@mac.com>
2022-04-17 01:02:36 -0500

    Updated changes

--- static/quiz.js
+++ static/quiz.js
@@ -1,26 +1,46 @@
 function makesymbol(){
     var symbol = $("<div class='symbol-style ui-widget-content'>")
-    symbol.draggable({revert: "invalid"});
+    symbol.draggable();
     $(symbol).html('<div>X</div>')
     $("#symbols").append(symbol)
 }
+
+function makeDroppableSpots() {
+    var chordLoc = $("<div class='chord-loc-style'>")
+    // $(chordLoc)[0].style('top', '800')
+    // console.log(chordLoc[0].classList)
+    chordLoc.droppable({ 
+        drop: function( event, ui ) {
+            var value = $(ui.draggable)
+
+            // Get dropped name
+            console.log(value) 
+
+
+
+        }
+    });
+    $("#all-chords").append(chordLoc)
+}
+
 
 
 
 
 $(document).ready(function() {
     makesymbol()
-    $( "#chordimage" ).droppable({ 
+    makeDroppableSpots()
+    // $( "#chord-loc" ).droppable({ 
 
-        drop: function( event, ui ) {
-            var value = $(ui.draggable)
+    //     drop: function( event, ui ) {
+    //         var value = $(ui.draggable)
 
-            // Get dropped name
-            console.log(value)
+    //         // Get dropped name
+    //         console.log(value)
 
            
-        }
-    });
+    //     }
+    // });
 
     
 })
